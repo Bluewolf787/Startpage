9611dbe72c8a8a349a6c118f947cf8835add644b
Bluewolf787 <dev.bluewolf@gmail.com>
2024-09-20 21:44:18 +0200

    SP-2: changed how time is displayed

--- src/app/pages/startpage/startpage.component.ts
+++ src/app/pages/startpage/startpage.component.ts
@@ -1,10 +1,9 @@
-import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-startpage',
   standalone: true,
-  imports: [DatePipe],
+  imports: [],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.scss',
 })
@@ -23,5 +22,28 @@ export class StartpageComponent implements OnInit {
     setInterval(() => {
       this.myDate = new Date();
     }, 1000);
+  }
+
+  showTime(): string {
+    const date = new Date();
+
+    const today = date.toLocaleString('en', { weekday: 'long' });
+    const hour = date.toLocaleString('pl', { hour: '2-digit' }); // use 24h time format
+    const minute = this.addZero(
+      date.toLocaleString('en', { minute: '2-digit' })
+    );
+    const second = this.addZero(
+      date.toLocaleString('en', { second: '2-digit' })
+    );
+    const day = date.toLocaleString('en', { day: '2-digit' });
+    const month = date.toLocaleString('en', { month: 'short' });
+    const year = date.toLocaleString('en', { year: 'numeric' });
+
+    return `${hour}:${minute}:${second} ${today} ${day}, ${month} ${year}`;
+  }
+
+  addZero(i: string): string {
+    if (i.length < 2) i = '0' + i;
+    return i;
   }
 }
